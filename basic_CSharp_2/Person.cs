20b72266dcfd395fdfde51465a4ad99d2ccf3afc
AliakbarTavangar <aliakbart.v1380@gmail.com>
2022-07-15 19:46:37 +0430

    basic_CSharp_2

---
+++ basic_CSharp_2/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basic_CSharp_2
+{
+    class Person
+    {
+        public string name;
+        public string family;
+        public string website;
+
+        public Person(string name, string family, string website)
+        {
+            this.name = name;
+            this.family = family;
+            this.website = website;
+        }
+    }
+}
---
+++ basic_CSharp_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace basic_CSharp_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the number of personnels:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Person[] people = new Person[number];
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"please enter person {i+1} name:");
+                string name = Console.ReadLine();
+                Console.WriteLine($"please enter person {i+1} family:");
+                string family = Console.ReadLine();
+                Console.WriteLine($"please enter person {i+1} website:");
+                string website = Console.ReadLine();
+
+                Person person = new Person(name, family, website);
+                people[i] = person;
+            }
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"name is: {person.name} family is: {person.family} website is: {person.website}");
+            }
+        }
+    }
+}
